4d85399f460719aaab02ff641ecafe61b97b67ef
Antonij Djajkoski <adzajkoski@gmail.com>
2022-08-05 10:46:18 +0200

    Removed default styling, adjustments

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import { store } from "./app/store";
+import { store } from "./store";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "./index.css";
 
-const container = document.getElementById("root")!;
-const root = createRoot(container);
+const container = document.getElementById("root");
+const root = createRoot(container as HTMLElement);
 
 root.render(
   <React.StrictMode>
